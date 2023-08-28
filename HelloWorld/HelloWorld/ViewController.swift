f3440a50f49e8090770cc88e56c0302afefb9550
서정원 <125368624+Seo-garden@users.noreply.github.com>
2023-08-28 17:07:23 +0900

    HelloWorld COMMIT

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 서정원 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblHello: UILabel!
+    
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
+
